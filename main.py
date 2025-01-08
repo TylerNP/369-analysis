724fea0473ac6e0b3bcb46ce338eb7dd46e7a8d2
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-08 02:51:04 -0800

    added counting for pixel coordinates and color

--- main.py
+++ main.py
@@ -1,67 +1,89 @@
 import csv 
 import sys
 import time
+import datetime
+from collections import defaultdict
 
 path = r"Data\2022_place_canvas_history.csv\2022_place_canvas_history.csv"
 
-class DateHour:
-    year : int
-    month : int
-    day : int
-    hour : int
-
-    def __init__(self, year : int, month : int, day : int, hour : int) -> "DateHour":
-        self.year = year
-        self.month = month
-        self.day = day
-        self.hour = hour
-
-    def cmp(self, other : "DateHour") -> bool:
-        return (self.year, self.month, self.day, self.hour) > (other.year, other.month, other.day, other.hour)
-
-    def print(self) -> None:
-        print(f"{self.year}-{self.month}-{self.day} {self.hour}")
-
-def str_to_datehour(date : str, time : str) -> DateHour:
+def arg_to_datetime(date : str, time : str) -> datetime:
     date_info = date.split("-")
-    return DateHour(date_info[0], date_info[1], date_info[2], time)
+    return datetime.datetime(int(date_info[0]), int(date_info[1]), int(date_info[2]), int(time))
 
-def most_placed_color_and_pixel(start : DateHour, end : DateHour) -> None:
-    pixels = {}
-    coords = {}
+def most_placed_color_and_pixel(start : datetime, end : datetime) -> None:
+    pixels = defaultdict(int)
+    coords = defaultdict(int)
+    start_time = time.perf_counter_ns()
+
     with open(path, "r", encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
         header_indices = {header: index for index, header in enumerate(headers)}
-        print(headers) 
-        for i, row in enumerate(csv_reader):
+        for row in csv_reader:
             tokens = row[header_indices['timestamp']].split(" ")
-            #color = row[header_indices['pixel_color']]
-            coord = row[header_indices['coordinate']]
             date_info = tokens[0].split("-")
             time_info = tokens[1].split(":")
-            timestamp = DateHour(date_info[0], date_info[1], date_info[2], time_info[0])
-            if timestamp.cmp(start) and coord == "73,1944":
-                #timestamp.print()
-                if i % 10 == 0:
-                    time.sleep(1)
-                print(tokens, coord)
-                print(i)
-            if timestamp.cmp(end):
+            timestamp = datetime.datetime(int(date_info[0]), int(date_info[1]), int(date_info[2]), int(time_info[0]))
+            if timestamp < start:
+                continue
+            if timestamp > end:
+                continue
+
+            color = row[header_indices['pixel_color']]
+            coord = row[header_indices['coordinate']]
+            pixels[color] += 1
+            coords[coord] += 1
+
+    end_time = time.perf_counter_ns()
+    print(f"Elapsed time: {end_time-start_time} ns\t {(end_time-start_time)/1000000} ms \t {(end_time-start_time)/1000000000} s")
+    if pixels:
+        print(f"Most Used Color {max(pixels, key=pixels.get)} {pixels[max(pixels, key=pixels.get)]}")
+        print(f"Most Changed Coord {max(coords, key=coords.get)} {coords[max(coords, key=coords.get)]}")
+        print(pixels)
+
+def read_within_timeframe(start : datetime, end : datetime, rows : int):
+    print(start, end)
+    start_time = time.perf_counter_ns()
+
+    with open(path, "r", encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        headers = next(csv_reader)
+        header_indices = {header: index for index, header in enumerate(headers)}
+        for i, row in enumerate(csv_reader):
+            if i > rows:
                 break
-            
-            #timestamp.print()
-            #timestamp = datetime.datetime.strptime(temp, "%y-%m-%d %H:%M:%S")
-            #print(timestamp)
-            #if timestamp >= start and timestamp <= end: 
-                #print("HI")
-            if i > 100000:
+            tokens = row[header_indices['timestamp']].split(" ")
+            date_info = tokens[0].split("-")
+            time_info = tokens[1].split(":")
+            timestamp = datetime.datetime(int(date_info[0]), int(date_info[1]), int(date_info[2]), int(time_info[0]))
+            if timestamp < start:
+                continue
+            if timestamp > end:
                 break
-            #print(f"{i}). {row}")
+            print(row)
+
+    end_time = time.perf_counter_ns()
+    print(f"Elapsed time: {end_time-start_time} ns\t {(end_time-start_time)/1000000} ms \t {(end_time-start_time)/1000000000} s")
+
+def read_n_rows(rows : int):
+    start_time = time.perf_counter_ns()
+
+    with open(path, "r", encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        headers = next(csv_reader)
+        header_indices = {header: index for index, header in enumerate(headers)}
+        for i, row in enumerate(csv_reader):
+            if i > rows:
+                break
+            print(row)
+
+    end_time = time.perf_counter_ns()
+    print(f"Elapsed time: {end_time-start_time} ns\t {(end_time-start_time)/1000000} ms \t {(end_time-start_time)/1000000000} s")
 
 if __name__ == '__main__':
     if len(sys.argv) > 3:
-        start = str_to_datehour(sys.argv[1], sys.argv[2])
-        end = str_to_datehour(sys.argv[3], sys.argv[4])
-        if end.cmp(start):
+        start = arg_to_datetime(sys.argv[1], sys.argv[2])
+        end = arg_to_datetime(sys.argv[3], sys.argv[4])
+        if end > start:
             most_placed_color_and_pixel(start, end)
+            #read_n_rows(10000000)
