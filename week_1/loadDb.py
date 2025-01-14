460cb629a670709dd2cd3ff4f513bacaaa49b716
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-13 22:08:20 -0800

    reordered code for organization

--- week_1/loadDb.py
+++ week_1/loadDb.py
@@ -18,39 +18,34 @@ c.execute('''
     )
     ''')
 
-def insertIntoTable():
-    start_time = time.perf_counter_ns()
-    chunk_size = 1000
-    with open(path, "r", encoding='utf-8') as file:
-        csv_reader = csv.reader(file)
-        headers = next(csv_reader)
-        header_indices = {header: index for index, header in enumerate(headers)}
-        chunk = []
-        for i, row in enumerate(csv_reader):
-            timestamp = row[header_indices['timestamp']]
-            color = row[header_indices['pixel_color']]
-            coord = row[header_indices['coordinate']].split(',')
-            chunk.append((coord[0], coord[1], color, timestamp))
-            if (i%chunk_size == 0):
-                #print(chunk)
-                c.executemany('''
-                    INSERT INTO pixels (x, y, color, timestamp)
-                        VALUES (?, ?, ?, ?)
-                    ''',
-                    chunk
-                )
-                conn.commit()
-                chunk = []
-
-    end_time = time.perf_counter_ns()
-
-    print(f"Seconds elapsed: {(end_time-start_time)/1000000000}")
-
-if (len(sys.argv) == 2):
-    if sys.argv[1] == '1':
-        insertIntoTable()
-
 c.execute('CREATE INDEX IF NOT EXISTS idx_timestamp ON pixels (timestamp)')
+
+start_time = time.perf_counter_ns()
+chunk_size = 1000
+with open(path, "r", encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    headers = next(csv_reader)
+    header_indices = {header: index for index, header in enumerate(headers)}
+    chunk = []
+    for i, row in enumerate(csv_reader):
+        timestamp = row[header_indices['timestamp']]
+        color = row[header_indices['pixel_color']]
+        coord = row[header_indices['coordinate']].split(',')
+        chunk.append((coord[0], coord[1], color, timestamp))
+        if (i%chunk_size == 0):
+            #print(chunk)
+            c.executemany('''
+                INSERT INTO pixels (x, y, color, timestamp)
+                    VALUES (?, ?, ?, ?)
+                ''',
+                chunk
+            )
+            conn.commit()
+            chunk = []
+
+end_time = time.perf_counter_ns()
+
+print(f"Seconds elapsed: {(end_time-start_time)/1000000000}")
 
 conn.commit()
 conn.close()
