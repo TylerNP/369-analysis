c3f9da9166353c59760afc009b69cafc266f7bb5
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-19 10:37:22 -0800

    redid analysis with polars

---
+++ week_2/polarsAnalysis.py
@@ -0,0 +1,26 @@
+import util
+import polars
+import time
+import sys
+
+def countMostCommon(min : int, max : int) -> None:
+    db = polars.read_parquet(util.db_path).filter(
+            (polars.col("timedelta") >= start) & (polars.col("timedelta") < end)
+        ).select(["color", "x", "y"])
+    color = db.group_by("color").agg(polars.len().alias("count")).top_k(k=1, by="count")
+    coord = db.group_by("x", "y").agg(polars.len().alias("count")).top_k(k=1, by="count")
+    
+    print(f"Most used color: #{(color["color"][0]):06x} ")
+    print(f"Most used coord: {coord["x"][0], coord["y"][0]}")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Not matching arguments <filename>.py YYYY-MM-DD HH YYYY-MM-DD HH")
+        exit()
+    start_time = time.perf_counter_ns()
+    start = util.min_hour_delta(sys.argv[1], sys.argv[2])
+    end = util.min_hour_delta(sys.argv[3], sys.argv[4])
+
+    countMostCommon(start, end)
+    end_time = time.perf_counter_ns()
+    print(f"Time elapsed: {(end_time-start_time)/1000000} ms")
---
+++ week_2/test_results_week_2_polars.md
@@ -0,0 +1,16 @@
+# Week 2 Results Polars
+## 1-Hour Timeframe
+- **Timeframe:** 2022-04-04 12 to 2022-04-04 13
+- **Execution Time:** 288.94575 ms
+- **Most Placed Color:** #FFFFFF
+- **Most Placed Pixel Location:** (0, 0)
+## 3-Hour Timeframe
+- **Timeframe:** 2022-04-04 12 to 2022-04-04 15
+- **Execution Time:** 401.197834 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** (0, 0)
+## 6-Hour Timeframe
+- **Timeframe:** 2022-04-04 12 to 2022-04-04 18
+- **Execution Time:** 624.527833 ms
+- **Most Placed Color:** #FFFFFF
+- **Most Placed Pixel Location:** (0, 0)
