ed08efbeacc377734f27dad8f85c7a68c18969a6
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-24 01:01:29 -0800

    added new program for data for week 3

--- other/csvToParquet.py
+++ other/csvToParquet.py
@@ -1,8 +1,8 @@
 import pyarrow.csv as pc
 import pyarrow.parquet as pq
 
-path = r"../data/lossy_2022_place_history.csv"
-write = r"../data/lossy_2022_place_history.parquet"
+path = r"../data/user_2022_place_history.csv"
+write = r"../data/user_2022_place_history.parquet"
 
 table = pc.read_csv(path)
 pq.write_table(table, write)
