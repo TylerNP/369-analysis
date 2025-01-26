e2a1293f5574e5be693fd5a53dd7dda1da2f1fbf
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2025-01-26 10:19:55 -0800

    removed old files that were moved

--- other/csvToParquet.py
+++
@@ -1,8 +0,0 @@
-import pyarrow.csv as pc
-import pyarrow.parquet as pq
-
-path = r"../data/user_2022_place_history.csv"
-write = r"../data/user_2022_place_history.parquet"
-
-table = pc.read_csv(path)
-pq.write_table(table, write)
